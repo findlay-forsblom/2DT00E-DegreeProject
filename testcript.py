e2c20642c9612bca232d6307df3c71135221124b
Findlay <findlay12@ymail.com>
2020-04-03 10:28:41 +0200

    real files

--- testcript.py
+++ testcript.py
@@ -40,7 +40,7 @@ filename = convert2Csv('./Datasets/humid','./Datasets/humid.csv', -2)
 luftFuktighet = pd.read_csv(filename)
 
 snowdepth.drop(['Tid (UTC)','Kvalitet'], axis=1,inplace = True)
-snowdepth.rename(columns={'Datum':'Date', 'Snö Djup':'Snow Depth'},inplace = True )
+snowdepth.rename(columns={'Datum':'Date', 'Snödjup':'Snow Depth'},inplace = True )
 
 airtemp.drop(['Från Datum Tid (UTC)','Till Datum Tid (UTC)', 'Kvalitet'], axis=1,inplace = True)
 airtemp.rename(columns={'Representativt dygn':'Date', 'Lufttemperatur':'Temp'},inplace = True )
@@ -51,52 +51,30 @@ fallAmount.rename(columns={'Representativt dygn':'Date', 'Nederbördsmängd':'Pr
 typeofFall.drop(['Från Datum Tid (UTC)','Till Datum Tid (UTC)', 'Kvalitet'], axis=1,inplace = True)
 typeofFall.rename(columns={'Representativt dygn':'Date', 'Nederbörd':'Precipitation'},inplace = True )
 
-
-
 luftFuktighet.rename(columns={'Datum':'Date', 'Relativ Luftfuktigh':'Luftfuktighet'},inplace = True )
+
 grouped = luftFuktighet.groupby('Date')
 luftFuktighet = grouped.mean()
 
-dfs = [snowdepth, airtemp, fallAmount, typeofFall, luftFuktighet]
+dfs = [snowdepth, airtemp, typeofFall, fallAmount, luftFuktighet]
 
 df_final = reduce(lambda left,right: pd.merge(left,right,on='Date', how='left'), dfs)
 
-df_final['Precipitation']= df_final['Precipitation'].fillna(value='none')
-df_final['Precip Amount']= df_final['Precip Amount'].fillna(value=0)
+dataset = df_final.copy()
 
-df_final[df_final['Temp'] < 10]
+dataset['Precipitation']= dataset['Precipitation'].fillna(value='none')
+dataset['Precip Amount']= dataset['Precip Amount'].fillna(value=0)
+dataset['Precip Amount'] = dataset['Precip Amount'] * (10**-3) 
 
-dataset = pd.merge(left=snowdepth, right=airtemp, left_on='Date', right_on='Date', how='outer')
-dataset = pd.merge(left=dataset, right=typeofFall, left_on='Date', right_on='Date', how='outer')
-dataset = pd.merge(left=dataset, right=fallAmount, left_on='Date', right_on='Date', how='outer')
+#test = df_final.copy()
+col = df_final['Snow Depth']
+ 
+col.drop(col.head(1).index,inplace=True)
+col= col.append(pd.Series([6]), ignore_index=True)
+
+dataset['Depth +day1'] = col
+dataset.drop(dataset.tail(1).index,inplace=True)
+dataset.dropna(inplace=True) #Drops only the missing temperature values
 
 
-snowdepth = snowdepth.iloc[:, [0, -1]].values
-airtemp = airtemp.iloc[:, [-2, -1]].values
-df1 = pd.DataFrame(airtemp, ['Date', 'Temp'])
-df2 = pd.DataFrame(snowdepth)
-df3 = pd.merge(df1, df2)
-merged_inner = pd.merge(left=df1, right=df2, left_on=0, right_on=0)
-    
-"""
-f = None
-with open('./Datasets/snowdepth.csv') as fp:
-  file = fp.readlines()
-  #lines = lines.replace(";", ",")
-  for row in file:
-      line = row.replace(";", ",")
-      line = line[:len(line)- 5]
-      f = open("demofile2.csv", "a")
-      f.write(line +'\n')
-      print(line)
-f.close()
-"""
-
-snowdepth = pd.read_csv('demofile2.csv')
-      
-"""
-snowdepth = pd.read_csv('demofile2.csv')
-fallamount = pd.read_csv('fallamount.csv')
-typeofFall = pd.read_csv('typeofFall.csv')
-airTemperature = pd.read_csv('airtemperature.csv')
-"""
+dataset[dataset['Temp'] < 10]
