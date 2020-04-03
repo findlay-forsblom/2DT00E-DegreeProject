a1bb508661f32d4404a07515a0dbaeb834ae2e36
Findlay <findlay12@ymail.com>
2020-04-03 11:57:01 +0200

    code clean up

--- testcript.py
+++ testcript.py
@@ -10,48 +10,30 @@ import pandas as pd
 import numpy as np
 from functools import reduce
 
-def convert2Csv(filename, writeFileName, num):
-    w = open(writeFileName, "w")
-    with open(filename) as fp:
-      file = fp.readlines()
-      #lines = lines.replace(";", ",")
-      for row in file:
-          line = row.replace(";", ",")
-          line = line.strip()
-          line = line[:num]
-          w.write(line +'\n')
-          print(line)
-    w.close()
-    return writeFileName 
+snowdepth = pd.read_csv('./Datasets/SnowDepth.csv', delimiter=';')
 
-filename = convert2Csv('./Datasets/snowdepth.csv','./Datasets/snowdepthnew.csv', -2)
-snowdepth = pd.read_csv(filename)
+airtemp = pd.read_csv('./Datasets/Airtemperature.csv', delimiter=';' )
 
-filename = convert2Csv('./Datasets/airtemperature.csv','./Datasets/airtemperatureNew.csv', -2)
-airtemp = pd.read_csv(filename)
+typeofFall = pd.read_csv('./Datasets/Precipitation.csv',  delimiter=';' )
 
-filename = convert2Csv('./Datasets/typeofFall.csv','./Datasets/typeofFallNew.csv', -2)
-typeofFall = pd.read_csv(filename)
+fallAmount = pd.read_csv('./Datasets/FallAmount.csv',delimiter=';')
 
-filename = convert2Csv('./Datasets/fallamount.csv','./Datasets/fallamountNew.csv', -2)
-fallAmount = pd.read_csv(filename)
+luftFuktighet = pd.read_csv('./Datasets/humidity.csv', delimiter=';')
 
-filename = convert2Csv('./Datasets/humid','./Datasets/humid.csv', -2)
-luftFuktighet = pd.read_csv(filename)
-
-snowdepth.drop(['Tid (UTC)','Kvalitet'], axis=1,inplace = True)
+snowdepth.drop(['Tid (UTC)','Kvalitet', 'Unnamed: 4', 'Unnamed: 5'], axis=1,inplace = True)
 snowdepth.rename(columns={'Datum':'Date', 'Snödjup':'Snow Depth'},inplace = True )
 
-airtemp.drop(['Från Datum Tid (UTC)','Till Datum Tid (UTC)', 'Kvalitet'], axis=1,inplace = True)
+airtemp.drop(['Från Datum Tid (UTC)','Till Datum Tid (UTC)', 'Kvalitet', 'Unnamed: 5', 'Unnamed: 6'], axis=1,inplace = True)
 airtemp.rename(columns={'Representativt dygn':'Date', 'Lufttemperatur':'Temp'},inplace = True )
 
-fallAmount.drop(['Från Datum Tid (UTC)','Till Datum Tid (UTC)', 'Kvalitet'], axis=1,inplace = True)
+fallAmount.drop(['Från Datum Tid (UTC)','Till Datum Tid (UTC)', 'Kvalitet', 'Unnamed: 5', 'Unnamed: 6'], axis=1,inplace = True)
 fallAmount.rename(columns={'Representativt dygn':'Date', 'Nederbördsmängd':'Precip Amount'},inplace = True )
 
-typeofFall.drop(['Från Datum Tid (UTC)','Till Datum Tid (UTC)', 'Kvalitet'], axis=1,inplace = True)
+typeofFall.drop(['Från Datum Tid (UTC)','Till Datum Tid (UTC)', 'Kvalitet', 'Unnamed: 5', 'Unnamed: 6'], axis=1,inplace = True)
 typeofFall.rename(columns={'Representativt dygn':'Date', 'Nederbörd':'Precipitation'},inplace = True )
 
-luftFuktighet.rename(columns={'Datum':'Date', 'Relativ Luftfuktigh':'Luftfuktighet'},inplace = True )
+luftFuktighet.drop(['Unnamed: 4', 'Unnamed: 5', 'Tid (UTC)','Kvalitet'], axis=1,inplace = True)
+luftFuktighet.rename(columns={'Datum':'Date', 'Relativ Luftfuktighet':'Luftfuktighet'},inplace = True )
 
 grouped = luftFuktighet.groupby('Date')
 luftFuktighet = grouped.mean()
@@ -63,7 +45,8 @@ df_final = reduce(lambda left,right: pd.merge(left,right,on='Date', how='left'),
 dataset = df_final.copy()
 
 dataset['Precipitation']= dataset['Precipitation'].fillna(value='none')
-dataset['Precip Amount']= dataset['Precip Amount'].fillna(value=0)
+dataset['Precip Amount']= dataset['Precip Amount'].fillna(value=dataset['Precip Amount'].mean())
+dataset['Luftfuktighet'] = dataset['Luftfuktighet'].fillna(value=dataset['Luftfuktighet'].mean()) 
 dataset['Precip Amount'] = dataset['Precip Amount'] * (10**-3) 
 
 #test = df_final.copy()
@@ -77,4 +60,4 @@ dataset.drop(dataset.tail(1).index,inplace=True)
 dataset.dropna(inplace=True) #Drops only the missing temperature values
 
 
-dataset[dataset['Temp'] < 10]
+dataset = dataset[dataset['Temp'] < 10]
