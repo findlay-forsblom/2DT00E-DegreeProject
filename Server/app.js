959e9b2a9473042530729fcaecb23b7ee3a2b6a8
LasseUlvatne <54929397+LasseUlvatne@users.noreply.github.com>
2020-04-15 13:03:26 +0200

    Initial implementation of ttn and nodemailer

--- Server/app.js
+++ Server/app.js
@@ -3,10 +3,11 @@ const hbs = require('express-hbs')
 const path = require('path')
 const mongoose = require('./config/mongoose.js')
 const dotenv = require('dotenv')
+const ttn = require('ttn')
 
-// dotenv.config({
-//   path: './.env'
-// })
+dotenv.config({
+  path: './.env'
+})
 
 const port = 8000
 
@@ -42,5 +43,64 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500)
   res.sendFile(path.join(__dirname, 'public', 'assets', 'html', '500.html'))
 })
+// const detections = {}
+
+// Listen for changes on application from TTN.
+// ttn.data(process.env.appID, process.env.accessKey)
+//   .then((client) => {
+//     client.on('uplink', async (devID, payload) => {
+//       const value = payload.payload_fields
+//       console.log(value, ': IS THE VALUE!!')
+
+//       // const isAck = value.includes('ack')
+
+//       // isAck ? console.log('Received ack from ', devID) : console.log('Received uplink from ', devID)
+
+//       // if (isAck) {
+//       //   // If ack was received => Extract message and delete from object.
+//       //   const ack = value.substring(3)
+//       //   delete detections[ack]
+//       // } else if (detections[value] === undefined) {
+//       //   // Notify video server and client through detectedLoRa function
+//       //   console.log('YAAAAY!')
+//       //   // detectedLora(STREAM_SERVER, client, io, payload, detections, value)
+//       // } else if (detections[value]) {
+//       //   // Add counts of detections. If 2 counts found send another ack.
+//       //   detections[value] = detections[value]++
+//       //   if (detections[value] > 2) {
+//       //     client.send(payload.dev_id, [value.substring(value.length - 2)])
+//       //     console.log('Sent new ack to node.')
+//       //   }
+//       // }
+//     })
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+// const nodeMailer = require('nodemailer')
+// const transporter = nodeMailer.createTransport({
+//   host: 'smtp.live.com',
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: 'ullvante_alf@hotmail.com',
+//     pass: process.env.email
+//   }
+// })
+// const mailOptions = {
+//   from: '"Lars" <ullvante_alf@hotmail.com>', // sender address
+//   to: 'ff222ey@student.lnu.se', // list of receivers
+//   subject: 'Hiho', // Subject line
+//   text: 'Test yo', // plain text body
+//   html: '<b>NodeJS Email Tutorial</b>' // html body
+// }
+
+// transporter.sendMail(mailOptions, (error, info) => {
+//   if (error) {
+//     return console.log(error)
+//   }
+//   console.log('EMAIL SENT!:D')
+// })
 
 app.listen(port, () => console.log('Server running at http://localhost:' + port))
