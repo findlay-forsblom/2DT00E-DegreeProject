76b1c56dbe1193dc7efe44ade5734fe164f53d06
LasseUlvatne <54929397+LasseUlvatne@users.noreply.github.com>
2020-04-22 15:56:10 +0200

    Fetch bookings from IBGO

--- Server/controllers/actionController.js
+++ Server/controllers/actionController.js
@@ -1,3 +1,19 @@
+'use strict'
+
+const axios = require('axios')
+
+function formatDate (date) {
+  var d = new Date(date)
+  var month = '' + (d.getMonth() + 1)
+  var day = '' + d.getDate()
+  var year = d.getFullYear()
+
+  if (month.length < 2) { month = '0' + month }
+  if (day.length < 2) { day = '0' + day }
+
+  return [year, month, day].join('%2D')
+}
+
 const actionController = {}
 
 // app.use('/action', require('./routes/actionRouter.js'))
@@ -39,7 +55,6 @@ actionController.index = async (req, res, next) => {
   const current = await Action.findOne({ id: req.params.id })
   const currentArr = []
   let currentContext
-  console.log(today.toDateString(), oneDay.toDateString(), twoDays.toDateString())
 
   if (current) {
     currentArr.push(current)
@@ -57,8 +72,6 @@ actionController.index = async (req, res, next) => {
     currentContext.currentAction[0].oneDay = oneDay.toDateString()
     currentContext.currentAction[0].twoDays = twoDays.toDateString()
   }
-
-  console.log(currentContext)
 
   // Must create new object to be able to render page with information.
   const context = {
@@ -79,8 +92,55 @@ actionController.clear = (req, res, next) => {
   res.send('Clear')
 }
 
-actionController.close = (req, res, next) => {
-  res.send('Close')
+actionController.close = async (req, res, next) => {
+  const fromDate = formatDate(req.params.date)
+  const toDate = fromDate
+  const url = `https://vaxjo.ibgo.se/BookingApi/GetBookings?start=${fromDate}+00%3A00&end=${toDate}+00%3A00&isPublic=true&resources%5B0%5D=50`
+  const bookings = await requestBookings(url)
+
+  // Print out the unique bookings.
+  bookings.forEach(booking => {
+    console.log(booking.start + '-' + booking.end + '. Team: ' + booking.description + '. Status: ' + booking.status)
+  })
+
+  res.redirect('back')
+
+  // const fetch = require('node-fetch')
+  // const data = await fetch(url)
+  // console.log(data.headers)
+  // config.headers = { Cookie: data.headers.get('set-cookie'), method: 'GET' }
+  // console.log(config)
+  // const bookings = await fetch(url, config)
+  // console.log(bookings)
+}
+
+async function requestBookings (url) {
+  const config = {
+    url: url
+  }
+  // Request for cookie
+  const data = await axios.request(config)
+  const cookie = data.headers['set-cookie'].toString()
+  config.headers = { Cookie: cookie }
+
+  // Request for bookings with cookie
+  const resp = await axios.request(config)
+  const bookings = resp.data.filter((booking) => {
+    return booking.status === 'booked'
+  })
+
+  // Remove duplicates in array.
+  const uniqueBookings = []
+  bookings.forEach(booking => {
+    const uniques = uniqueBookings.filter(item => {
+      return item.start === booking.start && item.description === booking.description
+    })
+    if (uniques.length === 0) {
+      uniqueBookings.push(booking)
+    }
+  })
+
+  return uniqueBookings
 }
 
 module.exports = actionController
--- Server/routes/actionRouter.js
+++ Server/routes/actionRouter.js
@@ -3,8 +3,8 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/clear/:id', controller.clear)
-router.get('/close/:id', controller.close)
+router.get('/clear/:id/:date', controller.clear)
+router.get('/close/:id/:date', controller.close)
 router.get('/:id', controller.index)
 
 module.exports = router
