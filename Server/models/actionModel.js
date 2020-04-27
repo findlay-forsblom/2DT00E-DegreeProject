d8e8729c6954b84f1b60ee49018abfaaf987426d
LasseUlvatne <54929397+LasseUlvatne@users.noreply.github.com>
2020-04-27 12:24:36 +0200

    IBGO data saved to database before page rendering

--- Server/models/actionModel.js
+++ Server/models/actionModel.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose')
 const actionSchema = mongoose.Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
-  forecast: { type: String, required: true },
   bookings: { type: String, required: true },
-  date: { type: String, required: true }
+  today: { type: String, required: true },
+  oneDay: { type: String, required: true },
+  twoDays: { type: String, required: true },
+  sensor: { type: String, required: true },
+  prediction: { type: String, required: true }
 })
 
 const Schema = mongoose.model('Action', actionSchema)
